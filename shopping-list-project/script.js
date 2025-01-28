3b4c49346129e36bf0ce0dfd2df7bff43e26ef04
Valdir Silva <valdirpiresba@gmail.com>
2025-01-28 18:30:10 -0300

    funcao para atualizar UI

--- shopping-list-project/script.js
+++ shopping-list-project/script.js
@@ -3,58 +3,82 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    const newItem = itemInput.value
+	const newItem = itemInput.value
 
-    // Validate Input
-    if (newItem === '') {
-        alert('Please add an item');
-        return;
-    }
+	// Validate Input
+	if (newItem === '') {
+		alert('Please add an item');
+		return;
+	}
 
-   // Create list item
-   const li = document.createElement('li');
-   li.appendChild(document.createTextNode(newItem));
+	// Create list item
+	const li = document.createElement('li');
+	li.appendChild(document.createTextNode(newItem));
 
-   const button = createButton('remove-item btn-link text-red');
-   li.appendChild(button);
+	const button = createButton('remove-item btn-link text-red');
+	li.appendChild(button);
 
-   itemList.appendChild(li);
+	// Add li to the DOM
+	itemList.appendChild(li);
 
-   itemInput.value = '';
+	checkUI();
+
+	itemInput.value = '';
 }
 
 function createButton(classes) {
-    const button = document.createElement('button');
-    button.className = classes;
-    const icon = createIcon('fa-solid fa-xmark');
-    button.appendChild(icon);
-    return button;
+	const button = document.createElement('button');
+	button.className = classes;
+	const icon = createIcon('fa-solid fa-xmark');
+	button.appendChild(icon);
+	return button;
 }
 
 function createIcon(classes) {
-    const icon = document.createElement('i');
-    icon.className = classes;
-    return icon;
+	const icon = document.createElement('i');
+	icon.className = classes;
+	return icon;
 }
 
 function removeItem(e) {
-    if (e.target.parentElement.classList.contains('remove-item')) {
-        e.target.parentElement.parentElement.remove();
-    }
+	if (e.target.parentElement.classList.contains('remove-item')) {
+		if (confirm('Are you sure?')) {
+			e.target.parentElement.parentElement.remove();
+
+			checkUI();
+		}
+	}
 }
 
 function clearItems(e) {
-    while (itemList.firstChild) {
-        itemList.removeChild(itemList.firstChild);
-    }
+	while (itemList.firstChild) {
+		itemList.removeChild(itemList.firstChild);
+	}
+
+	checkUI();
+}
+
+function checkUI() {
+	const items = itemList.querySelectorAll('li');
+
+	if (items.length === 0) {
+		clearBtn.style.display = 'none';
+		itemFilter.style.display = 'none';
+	} else {
+		clearBtn.style.display = 'block';
+		itemFilter.style.display = 'block';
+	}
 }
 
 // Event Listeners 
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+
+checkUI();
 
